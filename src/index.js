989606d9fa499219dd27c20a322008c56178c7ea
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2024-03-14 20:52:00 -0300

    fixing logical response bot

--- src/index.js
+++ src/index.js
@@ -11,10 +11,31 @@ client.on('qr', qr => {
   qrcode.generate(qr, { small: true })
 })
 
-client.on('message_create', message => {
-  if (message.body === '!ping') {
-    // send back "pong" to the chat the message was sent in
-    client.sendMessage(message.from, 'pong')
+client.on('message', message => {
+  //Verificando se a mensagem é de um grupo
+  if (message.from.includes('@c.us')) {
+    client.sendMessage(
+      message.from,
+      `Olá, eu sou o seu atendente virtual Max da loja Alefe Jóias! selecione uma das opções abaixo: \n 1 - Consultar saldo \n 2 - Extrato \n 3 - Transferência \n 4 - Pagamento \n 5 - Recarga \n 6 - Sair \n\n Digite o número correspondente a opção desejada.`
+    )
+  }
+  if (message.from.includes('@c.us') && message.body === '1') {
+    client.sendMessage(message.from, 'Digite o número da sua conta corrente')
+  }
+  if (message.from.includes('@c.us') && message.body === '2') {
+    client.sendMessage(message.from, 'Digite o número da sua conta corrente')
+  }
+  if (message.from.includes('@c.us') && message.body === '3') {
+    client.sendMessage(message.from, 'Digite o número da sua conta corrente')
+  }
+  if (message.from.includes('@c.us') && message.body === '4') {
+    client.sendMessage(message.from, 'Digite o número da sua conta corrente')
+  }
+  if (message.from.includes('@c.us') && message.body === '5') {
+    client.sendMessage(message.from, 'Digite o número da sua conta corrente')
+  }
+  if (message.from.includes('@c.us') && message.body === '6') {
+    client.sendMessage(message.from, 'Até a próxima!')
   }
 })
 
